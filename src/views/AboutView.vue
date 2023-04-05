65f38fffc3865f8bbc82b0fc6d6d7cce7ec7e43d
ro1l <qwer90218@gmail.com>
2023-04-05 18:06:26 +0800

    Form label must have an associated control

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,4 +2,15 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <DemoCard></DemoCard>
 </template>
+
+<script>
+import DemoCard from '@/components/DemoCard.vue';
+
+export default {
+  components: {
+    DemoCard,
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,20 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <VForm v-slot="{ errors, values, validate }" @submit="onSubmit">
+    {{ errors }} {{ values }}
+    <div class="mb-3">
+      <label for="email" class="form-label">
+        <VField id="email" name="email" type="email"
+      class="form-control" :class="{ 'is-invalid': errors['email'] }"
+      placeholder="請輸入 Email" rules="email|required" v-model="user.email"></VField>
+      Email</label>
+      <error-message name="email" class="invalid-feedback"></error-message>
+    </div>
+    <button class="btn me-2 btn-outline-primary" type="button" @click="validate">驗證</button>
+    <button class="btn btn-primary" type="submit">Submit</button>
+    </VForm>
 </template>
 
 <script>
@@ -13,6 +27,19 @@ export default {
   name: 'HomeView',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      user: {},
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user);
+    },
+  },
+  created() {
+    console.log(this);
   },
 };
 </script>
